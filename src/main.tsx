e6807e27195560d43ebaaa587307af3f6cbf2ac0
bonyoungkoo <gby1202@gmail.com>
2026-01-26 16:32:06 +0900

    chore: set up app entry and MSW environment
    - MSW service worker 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import { AppProviders } from './app/providers/AppProviders.tsx'
+import { router } from './app/router/Router.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function enableMocking() {
+  if (import.meta.env.MODE !== 'development') return
+  const { worker } = await import('@/mocks/browser.ts')
+  return worker.start({ onUnhandledRequest: 'bypass' })
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <AppProviders>
+        <RouterProvider router={router} />
+      </AppProviders>
+    </StrictMode>,
+  )
+})
